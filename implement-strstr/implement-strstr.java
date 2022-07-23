767952f93ff4d8dcce7f1ed150763a30d9f8d5a0
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2022-07-23 10:34:52 +0200

    Time: 1 ms, Memory: 42.2 MB - LeetHub

--- implement-strstr/implement-strstr.java
+++ implement-strstr/implement-strstr.java
@@ -3,9 +3,19 @@ class Solution {
         int len1 = haystack.length();
         int len2 = needle.length();
         
-        if (len2==0)
+        if (haystack==null || needle==null)
             return 0;
         
-        return haystack.indexOf(needle);
+        for (int i=0; i<len1; i++){
+            int count = 0;
+            while ( count < len2 && count+i < len1 && haystack.charAt(count+i) == needle.charAt(count))
+                count++;
+            if (count == len2 )
+                return i;
+        }
+        
+        return -1;
+        
+        
     }
 }
