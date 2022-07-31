4e6df2d3891a31bb061af9c495824448a6691379
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2022-07-31 16:58:38 +0200

    Time: 1 ms, Memory: 45.5 MB - LeetHub

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -17,9 +17,11 @@ public class Solution {
         if ( head == null || head.next == null ) return false;
         
         while ( fast.next != null && fast.next.next != null ){
-            fast = fast.next.next;
-            slow = slow.next;
-            if ( fast == slow ) return true;          
+            fast = fast.next;
+            
+            if ( fast == slow ) return true;  
+            fast = fast.next;
+            slow = slow.next;        
         }
         return false;
 }
