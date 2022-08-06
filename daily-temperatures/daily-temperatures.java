f4fd0eb7ed1d1541cb9f7af208680359391914f9
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2022-08-06 18:57:11 +0200

    Time: 149 ms, Memory: 55 MB - LeetHub

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -3,10 +3,8 @@ class Solution {
         int[] ans = new int [temp.length];
         Stack<Integer> st = new Stack<Integer>();
         for( int i=0; i<temp.length; i++){
-                while ( !st.empty() && temp[st.peek()] < temp[i]  ){
-                    ans[st.peek()] = i-st.peek();
-                    st.pop();
-                }
+                while ( !st.empty() && temp[st.peek()] < temp[i]  )
+                    ans[st.peek()] = i-st.pop();
                 st.push(i);
             }
         return ans;
