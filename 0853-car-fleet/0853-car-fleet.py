1152e578c00011443d6bff8c21e934a7a03c6142
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2023-01-18 17:23:25 +0200

    Time: 1142 ms (44.28%), Space: 34 MB (71.42%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,25 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        combine_list = list()
-        fromTarget = list()
-        answer = 0
-        for x,y in zip( position , speed ):
-            combine_list.append( (x,y) )
-        combine_list = sorted( combine_list )
-        for (x,y) in combine_list:
-            fromTarget.append((target - x) / y)
+        posAndSpeed = [[s,p] for s,p in zip( position , speed)]
+        stack = list()
 
-        fromTarget = [target] + fromTarget
-        i = len(fromTarget) - 1
-        while( i > 0):
-            if ( fromTarget[i] < fromTarget[i-1]):
-                answer += 1
-                i -= 1
-            else:
-                x = fromTarget[i]
-                i -= 1
-                while( i > 0 and x >= fromTarget[i]):
-                    i -= 1
-                answer += 1    
-
-        return answer
+        for p, s in sorted(posAndSpeed)[::-1]:
+            stack.append( (target-p) / s)
+            if ( len(stack) >= 2 and stack[-1] <= stack[-2]):
+                stack.pop()
+        return len(stack)
