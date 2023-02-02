07b62295bbbe57064a29b807c90be71ae4d942f1
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2023-02-02 03:00:37 +0200

    Time: 39 ms (69.48%), Space: 14 MB (31.27%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        # dictionary to give each letter an ordered index
+        letter_to_index = {}
+        for i in range(len(order)):
+            letter_to_index[order[i]] = i
+
+        #check the order of two words 
+        def isOrdered(w1: str, w2: str) -> bool:
+            for x, y in zip( w1, w2):
+                if letter_to_index[x] == letter_to_index[y]:
+                    continue
+                if letter_to_index[x] > letter_to_index[y]:
+                    return False
+                else:
+                    return True
+            return True if len(w1) <= len(w2) else False
+
+        for i in range(1, len(words)):
+            if not isOrdered(words[i-1] , words[i]):
+                return False
+        return True        
