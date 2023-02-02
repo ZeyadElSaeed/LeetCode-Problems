77a94e93f2f4a999c8de19ec65da62e446acb65e
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2023-02-02 03:10:19 +0200

    Time: 59 ms (33.01%), Space: 14.1 MB (68.22%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        answer = []
+        for i in range(1,  len(nums) +1 ):
+            answer.append(sum(nums[:i]))
+        return answer
