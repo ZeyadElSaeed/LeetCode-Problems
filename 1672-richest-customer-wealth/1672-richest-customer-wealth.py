f2d3251884be3e31f7f00a2a20d0fe53fddd3ab4
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2023-02-11 07:13:42 +0200

    Time: 126 ms (6.22%), Space: 31.7 MB (23.56%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -1,9 +1,7 @@
+import numpy as np
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        maximum_wealth = 0
-        for account in accounts:
-            maximum_wealth = max( maximum_wealth, sum(account))
-        return maximum_wealth
+        return np.max( np.sum(accounts, axis=1) )
 
     
     
