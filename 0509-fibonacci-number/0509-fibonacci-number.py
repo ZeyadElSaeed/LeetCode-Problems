80d3f6f17bc3dc087fb577f99d5485aec5e46dd0
Zeyad ElSaeed <70777445+ZeyadElSaeed@users.noreply.github.com>
2023-02-11 06:24:41 +0200

    Time: 671 ms (29.26%), Space: 13.9 MB (45.15%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,21 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return n
-        dp = [0] * (n+1)
-        dp[1] = 1
         
-        for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-            
-        return dp[n]
+        # using Recursion
+        def recursion(n):
+            if n == 0 or n == 1:
+                return n
+            return recursion(n-1) + recursion(n-2)
+        
+        # using dynamic programming
+        def dynamic(n):
+            if n == 0 or n == 1:
+                return n
+            dp = [0]*(n+1)
+            dp[1] = 1
+            for i in range(2, n+1):
+                dp[i] = dp[i-1] + dp[i-2]
+            return dp[n]
+        
+        return recursion(n)
         
